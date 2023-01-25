0e8d3561c2779345bc4016c089333b79da0f0236
GetPsyched <priyanshutr@gmail.com>
2023-01-25 21:19:38 +0530

    Add hyperlink to /courses

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Link from 'next/link';
+import CoursesPageCard from './courses/(card)';
 
 export default function HomePage() {
   return (
@@ -7,17 +8,29 @@ export default function HomePage() {
         <h1>NIT-KKR Student Support System</h1>
         <h2>v1-alpha</h2>
         <p>
-          Welcome! The <Link href="/about">NKSSS team</Link> aims to solve as many
-          issues you face in our college as technologically possible! The
+          Welcome! The <Link href="/about">NKSSS team</Link> aims to solve as
+          many issues you face in our college as technologically possible! The
           following 3 options are ones that we're currently actively developing.
           This website is currently under development and you will be notified
           once its full release rolls around!
           <br />
           <br />
-          <strong>PS:</strong> NKSSS is <em>NOT</em> affiliated with NITKKR and is completely
-          student run.
+          <strong>PS:</strong> NKSSS is <em>NOT</em> affiliated with NITKKR and
+          is completely student run.
         </p>
       </header>
+
+      <main>
+        <nav>
+          <ol>
+            <Link href="/courses">
+              <li>
+                <CoursesPageCard />
+              </li>
+            </Link>
+          </ol>
+        </nav>
+      </main>
     </div>
   );
 }
