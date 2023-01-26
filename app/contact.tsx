7755fbe12f5f685ca520520b5a882a2124166d31
GetPsyched <priyanshutr@gmail.com>
2023-01-26 17:06:49 +0530

    Add contact component

---
+++ app/contact.tsx
@@ -0,0 +1,12 @@
+export default function ContactInfo() {
+  return (
+    <address>
+      <a href="https://discord.gg/3P3wg3Yahp">Discord</a>
+      <br />
+      <a href="https://instagram.com/nksss.live">Instagram</a>
+      <br />
+      <a href="https://facebook.com/nksss.live">Facebook</a>
+      <br />
+    </address>
+  );
+}
