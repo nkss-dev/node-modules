9c733516ff909c5cdb4052a85d248dcbf1cd2bcc
GetPsyched <priyanshutr@gmail.com>
2023-01-28 00:57:00 +0530

    Fix: Update dir of /courses
    This was done to avoid the layout.tsx of /courses being inherited to /course/[code] as well

---
+++ app/courses/(non-code)/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Courses</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="Enter a branch and semester to see a given semester's credit distribution and links to all of its required/optional courses. For each course, a segmented display is provided of its content, outcomes, and more. More than just static content, this webpage will link you to corresponding notes, past papers, and reference books."
+      />
+    </>
+  );
+}
---
+++ app/courses/(non-code)/page.tsx
@@ -0,0 +1,13 @@
+import FilteredCourses from '../components/filtered-courses';
+
+async function fetchCourses() {
+  const response = await fetch(`https://api.nksss.live/courses`);
+  const courses = await response.json();
+  return courses.data as Array<Course>;
+}
+
+export default async function CoursesPage() {
+  const courses = await fetchCourses();
+
+  return <FilteredCourses courses={courses} />;
+}
