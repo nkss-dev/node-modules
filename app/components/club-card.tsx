5b927ae3ae2dcc6749b848f63e732551e109b396
GetPsyched <priyanshutr@gmail.com>
2023-01-31 17:22:05 +0530

    Add email and description to club-card

--- app/components/club-card.tsx
+++ app/components/club-card.tsx
@@ -2,21 +2,30 @@ import Link from 'next/link';
 import ImageWithFallback from './fallback-image';
 
 export default function ClubCard({ club }: { club: Club }) {
+  const email: string | undefined = club.socials.find(
+    (social) => social.platform == 'email'
+  )?.link;
+
   return (
     <Link href={`/clubs/${club.short_name}`}>
-      <figure>
-        <ImageWithFallback
-          src={`/assets/clubs/${club.short_name}/logo.png`}
-          width={100}
-          height={100}
-          alt="club logo"
-          style={{ borderRadius: '50%' }}
-        />
-        <figcaption>
-          <h3>{club.name}</h3>
-          <p>{club.description}</p>
-        </figcaption>
-      </figure>
+      <article>
+        <header>
+          <ImageWithFallback
+            src={`/assets/clubs/${club.short_name}/logo.png`}
+            width={85}
+            height={85}
+            alt="club logo"
+            style={{ borderRadius: '50%', height: '85px' }}
+          />
+          <hgroup>
+            <h3>{club.name}</h3>
+            <address>
+              <small>{email ? <p>{email}</p> : <></>}</small>
+            </address>
+          </hgroup>
+        </header>
+        <p>{club.description}</p>
+      </article>
     </Link>
   );
 }
