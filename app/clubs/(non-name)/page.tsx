d429d51ba14f769fa8c0478b655ec8936ae46793
GetPsyched <priyanshutr@gmail.com>
2023-02-12 18:13:06 +0530

    Add clubs page

--- app/clubs/(non-name)/page.tsx
+++ app/clubs/(non-name)/page.tsx
@@ -1,1 +1,45 @@
-export default function ClubsPage() {}
+import { Fragment } from 'react';
+import ClubsPageCard from '../../components/club-card';
+
+const clubCategories = [
+  'Cultural Club',
+  'Magazine',
+  'Technical Club',
+  'Technical Society',
+] as const;
+
+async function fetchClubs() {
+  const clubsResponse = await fetch('https://api.nksss.live/clubs');
+  return (await clubsResponse.json()).data;
+}
+
+const getCategorisedClubs = (
+  category: ClubCategory,
+  clubs: Array<ClubBasic>
+) => {
+  const filteredClubs = clubs.filter((club) => club.category === category);
+
+  if (filteredClubs.length === 0)
+    return <p>No club found for the {category} category</p>;
+
+  return filteredClubs.map((club, index) => (
+    <li key={index}>
+      <ClubsPageCard club={club} />
+    </li>
+  ));
+};
+
+export default async function ClubsPage() {
+  const clubs: Array<ClubBasic> = await fetchClubs();
+
+  return clubCategories.map((category: ClubCategory, categoryIndex) => {
+    return (
+      <Fragment key={categoryIndex}>
+        <h2>{category}</h2>
+        <ol className="flex flex-row flex-wrap">
+          {getCategorisedClubs(category, clubs)}
+        </ol>
+      </Fragment>
+    );
+  });
+}
