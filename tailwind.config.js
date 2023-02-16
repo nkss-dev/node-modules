0153ccfe912ad9383202dd7c2afc6f31c141fd45
GetPsyched <priyanshutr@proton.me>
2023-02-16 15:40:07 +0530

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: 'Poppins, sans-serif',
+    },
+    extend: {
+      colors: {
+        discord: {
+          100: '#36393f',
+          200: '#2f3136',
+        },
+        hyperlink: '#0cbc8b'
+      },},
   },
   plugins: [],
-}
+};
