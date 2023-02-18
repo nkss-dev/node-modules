fae2c1020a70ddaae9a9de7a41d1fca0af930aaa
GetPsyched <priyanshutr@proton.me>
2023-02-18 16:16:14 +0530

    Add hyperlink class

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,17 @@
 export default function ContactInfo() {
   return (
     <address>
-      <a href="https://discord.gg/3P3wg3Yahp">Discord</a>
+      <a className="hyperlink" href="https://discord.gg/3P3wg3Yahp">
+        Discord
+      </a>
       <br />
-      <a href="https://instagram.com/nksss.live">Instagram</a>
+      <a className="hyperlink" href="https://instagram.com/nksss.live">
+        Instagram
+      </a>
       <br />
-      <a href="https://facebook.com/nksss.live">Facebook</a>
+      <a className="hyperlink" href="https://facebook.com/nksss.live">
+        Facebook
+      </a>
       <br />
     </address>
   );
