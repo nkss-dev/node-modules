24435ba2e7d55cdb7aebe9132a2557e9d8de6b80
GetPsyched <priyanshutr@proton.me>
2023-03-03 12:32:46 +0530

    Add pacman loading animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+import PacmanLoader from 'react-spinners/PacmanLoader';
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <PacmanLoader
+      className="m-auto"
+      color="yellow"
+      loading={loading}
+      size={25}
+    />
+  );
 }
