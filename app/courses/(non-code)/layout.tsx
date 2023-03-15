da68fbf911dba60446510f83ce686a55f07b6f54
GetPsyched <priyanshutr@proton.me>
2023-03-15 14:32:56 +0530

    Add prompt

--- app/courses/(non-code)/layout.tsx
+++ app/courses/(non-code)/layout.tsx
@@ -1,10 +1,9 @@
-import Balancer from 'react-wrap-balancer';
 import DefaultLayout from '../../../components/default-layout';
 
 export const metadata = {
   title: 'Courses',
   description:
-    "Check out all the courses with optional branch and semester filters for a better view at the credit distribution.",
+    'Check out all the courses with optional branch and semester filters for a better view at the credit distribution.',
 };
 
 export default async function CoursesLayout({
@@ -14,9 +13,10 @@ export default async function CoursesLayout({
 }) {
   return (
     <DefaultLayout
-      title='Courses'
-      description='Choose your branch and semester below to display the respective courses!'
-      prompt='This page will soon, with the v1 release, display as many of the 400+ courses that our college offers for their B. Tech. degree.'
+      title="Courses"
+      description="Choose your branch and semester below to display the respective courses!"
+      prompt="We're still in the process of adding the descriptions of more courses! Open electives, viva, project, and lab courses are yet to be added."
+      alert={true}
     >
       {children}
     </DefaultLayout>
