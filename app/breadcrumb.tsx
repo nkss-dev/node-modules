8128634aa6c4a1e547ec1d1a957ac6707fcaa8ae
GetPsyched <priyanshutr@proton.me>
2023-03-18 19:26:25 +0530

    Add quick list

--- app/breadcrumb.tsx
+++ app/breadcrumb.tsx
@@ -4,9 +4,9 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSelectedLayoutSegments } from 'next/navigation';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { BiBell } from 'react-icons/bi';
-import { BsPersonFill } from 'react-icons/bs';
+import { BsLink45Deg, BsPersonFill } from 'react-icons/bs';
 
 const capitalise = (text: string) => {
   return text
@@ -19,8 +19,8 @@ export default function Breadcrumb() {
     (segment) => !(segment.startsWith('(') && segment.endsWith(')'))
   );
 
-  if (segments.length === 0)
-    return <header className="mb-4 md:mb-8"></header>;
+  const [showQuickList, setShowQuickList] = useState(false);
+
   return (
     <nav
       aria-label="Breadcrumb"
@@ -68,6 +68,77 @@ export default function Breadcrumb() {
             'inline-flex items-center justify-end'
           )}
         >
+          <li
+            className="p-1 relative"
+            onMouseEnter={() => {
+              setShowQuickList(true);
+            }}
+            onMouseLeave={() => {
+              setShowQuickList(false);
+            }}
+          >
+            <BsLink45Deg
+              className={clsx('h-5 w-5', 'sm:h-6 sm:w-6', 'md:h-7 md:w-7')}
+            />
+            {showQuickList && (
+              <nav className="absolute right-0 bg-palette-500 min-w-max mt-1 rounded-md shadow-lg">
+                <ul className="py-2">
+                  <li className='mb-3'>
+                    <p className="px-3">Academic Calendars</p>
+                    <ul className='text-palette-300'>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1iJ6BxMBIqZEfFMe9c4ocidcm_madI13U">
+                          Revised Academic Calendar
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li className='mb-3'>
+                    <p className="px-3">Syllabi</p>
+                    <ul className='text-palette-300'>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/16GZfj3pzUaiKMx-frey_K2dB2Oy6xe0d">
+                          Civil Engineering
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1sgjxNv4gf662kwbNeAGygPD49NvGQ2gL">
+                          Computer Science
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1IUDACGOHCO3h-vhx-G8ofwrD3LUfqTP-">
+                          Electronics And Communication Engineering
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1m-xhRr2J-kK-5EUBeRj7M33ipndCfT_t">
+                          Electrical Engineering
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1hF5sqQb6ifLHcZrc2immFpSn6IoF35OD">
+                          Information Technology
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1vvgCKCY8hTbHd4oFT51MRXdyYuT2lSLI">
+                          Mechanical Engineering
+                        </a>
+                      </li>
+                      <li className="hover:text-palette-100 list-disc list-inside px-3">
+                        <a href="https://drive.google.com/file/d/1l--XXOVAGTyhZYGxz_-G6RJT_Bq-FKY9">
+                          Production and Industrial Engineering
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            )}
+          </li>
+
           <li className="p-1">
             <BiBell
               className={clsx('h-5 w-5', 'sm:h-6 sm:w-6', 'md:h-7 md:w-7')}
