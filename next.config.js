1d18865e1bf902256fe29905af920a5d5bbee43c
GetPsyched <priyanshutr@proton.me>
2023-09-24 23:43:50 +0530

    feat: implement signin, signout in the breadcrumb

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
   reactStrictMode: true,
 };
