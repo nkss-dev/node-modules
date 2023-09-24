e87db54086abdf996df3c6dbefd2df0a52a05327
GetPsyched <priyanshutr@proton.me>
2023-09-24 23:52:30 +0530

    feat: use authorisation token

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,5 +1,10 @@
 export const fetcher = async (url: URL | RequestInfo) => {
-  const response = await fetch(process.env.NEXT_PUBLIC_BREADBOARD_URL! + url);
+  const response = await fetch(process.env.NEXT_PUBLIC_BREADBOARD_URL! + url, {
+    credentials: 'include',
+    headers: {
+      Authorization: `Bearer ${process.env.BREADBOARD_TOKEN}`,
+    },
+  });
   const responseJson = await response.json();
 
   if (!response.ok) {
