5d538ef77452fdbad291d57c9e952d39f93b1a98
GetPsyched <priyanshutr@proton.me>
2023-09-29 15:42:09 +0530

    feat: add post route

--- utils/api-actions.ts
+++ utils/api-actions.ts
@@ -1,4 +1,4 @@
-export const get = async (url: URL | RequestInfo) => {
+const get = async (url: URL | RequestInfo) => {
   const response = await fetch(process.env.NEXT_PUBLIC_BREADBOARD_URL! + url, {
     credentials: 'include',
     headers: {
@@ -16,4 +16,24 @@ export const get = async (url: URL | RequestInfo) => {
   return responseJson.data;
 };
 
-export default { GET: get };
+const post = async (url: URL | RequestInfo, body: any) => {
+  const response = await fetch(process.env.NEXT_PUBLIC_BREADBOARD_URL! + url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    credentials: 'include',
+    headers: {
+      Authorization: `Bearer ${process.env.BREADBOARD_TOKEN}`,
+    },
+  });
+  const responseJson = await response.json();
+
+  if (!response.ok) {
+    const error = new Error('An error has occured while updating data.');
+    error.cause = responseJson.errors;
+    throw error;
+  }
+
+  return responseJson.data;
+};
+
+export default { GET: get, POST: post };
